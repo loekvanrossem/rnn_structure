b417486ff784e98038525c2c8c02a8a706cb7157
Loek van Rossem <loekvanrossem@gmail.com>
2022-11-07 20:16:28 +0000

    moving points through time

--- visualization.py
+++ visualization.py
@@ -4,8 +4,11 @@ import matplotlib.pyplot as plt
 from sklearn.manifold import MDS
 from sklearn.decomposition import PCA
 
+from ipywidgets import Layout, interact, IntSlider
+from IPython.display import display, clear_output
 
-def hidden_repr(hidden_states: pd.DataFrame, transform="PCA", n_labels=10):
+
+def hidden_repr(hidden_states: pd.DataFrame, transform="PCA", n_labels=10, fig_size=5):
     """
     Show a low dimensional representation of the hidden state dynamics
 
@@ -17,6 +20,8 @@ def hidden_repr(hidden_states: pd.DataFrame, transform="PCA", n_labels=10):
         The type of dimensionality reduction to use: PCA or MDS
     n_labels : int, default 10
         The number of epochs which will be labeled
+    fig_size : float, default 5
+        The size of the figure
     """
     # Apply dimensionality reduction
     index = hidden_states.index
@@ -28,9 +33,13 @@ def hidden_repr(hidden_states: pd.DataFrame, transform="PCA", n_labels=10):
         x = pca.fit_transform(hidden_states)
     x = pd.DataFrame(x, index=index)
 
+    # Plot
+    fig = plt.figure(figsize=(fig_size, fig_size))
+    ax = fig.add_subplot(111)
+    points = []
     for dataset_name, dataset in x.groupby("Dataset"):
         for input_name, input in dataset.groupby("Input"):
-            plt.plot(input[0], input[1], label=input_name, zorder=0)
+            ax.plot(input[0], input[1], label=input_name, zorder=0)
             # Label some points
             first_epoch = hidden_states.index.get_level_values("Epoch").min()
             last_epoch = hidden_states.index.get_level_values("Epoch").max()
@@ -41,13 +50,34 @@ def hidden_repr(hidden_states: pd.DataFrame, transform="PCA", n_labels=10):
                 base=np.e,
             ).astype(int)
             for epoch in log_range:
-                plt.scatter(input[0][epoch], input[1][epoch], s=10, c="Black", zorder=1)
-                plt.annotate(
+                ax.scatter(input[0][epoch], input[1][epoch], s=10, c="Black", zorder=1)
+                ax.annotate(
                     epoch,
                     (input[0][epoch] + 0.02, input[1][epoch] + 0.02),
                     zorder=2,
                 )
-    plt.legend(loc="upper left")
-    plt.show()
+            # Add time moveable points
+            point = ax.plot(
+                input[0][first_epoch], input[1][first_epoch], "ko", alpha=0, zorder=3
+            )
+            points.append((point, input))
+    plt.legend(bbox_to_anchor=(1.05, 1.0), loc="upper left")
 
-    return x
+    # Add slider for time movable points
+    int_wdgt = IntSlider(
+        description="Epoch:",
+        value=first_epoch,
+        min=first_epoch,
+        max=last_epoch,
+        step=1,
+        layout=Layout(width=f"{int(fig_size*7)}%"),
+    )
+
+    @interact(epoch=int_wdgt)
+    def slider_epochs(epoch):
+        for point, input in points:
+            point[0].set_xdata(input[0][epoch])
+            point[0].set_ydata(input[1][epoch])
+            point[0].set_alpha(1)
+        display(fig)
+        clear_output(wait=True)
