8bd9a1ad8e2c039a82e3e7273a2857ab7b6d6e60
Loek van Rossem <loekvanrossem@gmail.com>
2024-01-03 10:32:38 +0000

    tuned

--- DNN/models.py
+++ DNN/models.py
@@ -110,7 +110,7 @@ class CNN(MLP):
         super(MLP, self).__init__()
 
         kernel_size = 11
-        n_channels = 2
+        n_channels = 50
 
         self.device = device
         self.non_linearity = non_linearity
