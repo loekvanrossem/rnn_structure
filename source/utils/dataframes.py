849db36f78f908b738173e75eb1300443d324218
Loek van Rossem <loekvanrossem@gmail.com>
2024-03-18 12:52:51 +0000

    Increased efficiency

--- source/utils/dataframes.py
+++ source/utils/dataframes.py
@@ -4,25 +4,21 @@ import pandas as pd
 
 def to_ndarray(df: pd.DataFrame) -> np.ndarray:
     """
-    Convert a multiindexed pandas dataframe to a multidimensional array.
+    Convert a multi-indexed pandas dataframe to a multidimensional array.
 
     Returns
     -------
     array : np.ndarray
-        An array containting the dataframe entries
-        each dimensions of the array corresponds to an index of the dataframe
-        and the final dimension corresponding to the column index
+        An array containing the dataframe entries
+        each dimension of the array corresponds to an index of the dataframe
+        and the final dimension corresponds to the column index
     """
     if df.index.nlevels == 1:
         return df.to_numpy()
 
-    a = []
-    for x in df.groupby(level=0):
-        data = x[1].droplevel(level=0)
-        a.append(to_ndarray(data))
-
-    a = np.array(a)
-    return a
+    groups = df.groupby(level=0)
+    sub_arrays = [to_ndarray(group.droplevel(level=0)) for _, group in groups]
+    return np.array(sub_arrays)
 
 
 def to_labels(df: pd.DataFrame) -> list[np.ndarray]:
@@ -31,36 +27,12 @@ def to_labels(df: pd.DataFrame) -> list[np.ndarray]:
     Returns
     -------
     labels : list[np.ndarray]
-        An array for each level containting the labels
-        each dimensions of the array corresponds to an index of the dataframe
+        An array for each level containing the labels
+        each dimension of the array corresponds to an index of the dataframe
         ordering is consistent with to_ndarray function"""
-    nlevels = df.index.nlevels
+    df = df.copy()
     labels = []
-    for level in range(nlevels):
-        label = df.copy().iloc[:, 0:1]
-        for index, entry in label.iterrows():
-            if nlevels == 1:
-                label.loc[index] = index
-            else:
-                label.loc[index] = index[level]
-        labels.append(to_ndarray(label).squeeze(axis=-1))
+    for level in range(df.index.nlevels):
+        label = df.index.get_level_values(level).to_numpy()
+        labels.append(label)
     return labels
-
-
-# def to_ndarray(df: pd.DataFrame) -> np.ndarray:
-#     """
-#     Convert a multiindexed pandas dataframe to a multidimensional array.
-
-#     Returns
-#     -------
-#     array : np.ndarray
-#         An array containting the dataframe entries
-#         each dimensions of the array corresponds to an index of the dataframe
-#         and the final dimension corresponding to the column index
-#     """
-
-#     dims = [len(df.index.get_level_values(i).unique()) for i in range(df.index.nlevels)]
-#     dims = dims + [-1]
-#     array = np.reshape(df.to_numpy(), dims)
-
-#     return array
